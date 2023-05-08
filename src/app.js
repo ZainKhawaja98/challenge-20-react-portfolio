ed4a256e7fa6f6e4996a8f8091f89fc59ff0be7c
zain khawaja <zainkhawaja98@gmail.com>
2023-05-08 19:19:04 -0400

    updatd app.js and index.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HomeBar from "./components/Navbar";
+import Home from "./pages/Home";
+import Applications from "./pages/Applicaitons";
+import Contact from "./pages/Contact"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router basename="/Portfolio">
+       <HomeBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Applications" element={<Applications />} />
+          <Route exact path="/Contact" element={<Contact />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
